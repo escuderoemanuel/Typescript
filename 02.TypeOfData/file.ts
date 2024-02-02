31f90061c7fe6ac2ee19ee90b8ec92a3a1162796
Emanuel Escudero <escudero.mdz@gmail.com>
2024-02-02 01:05:17 -0300

    Updated

---
+++ 02.TypeOfData/file.ts
@@ -0,0 +1,31 @@
+// Tipos de datos
+
+// Tipado por 'inferencia'
+const person = 'Emanuel' // -> Por 'inferencia' TS sabe que es un string
+const age = 38 // -> Por 'inferencia' TS sabe que es un number
+const isStudent = true // -> Por 'inferencia' TS sabe que es un boolean
+const hobbies = ['Train', 'Play the Guitar'] // -> Por 'inferencia' TS sabe que es un array de strings
+const address = {
+  street: 'Main St',
+  city: 'New York'
+} // -> Por 'inferencia' TS sabe que es un objeto
+
+// Tipado explicito
+const person2: string = 'Emanuel'
+const age2: number = 38
+const isStudent2: boolean = true
+const hobbies2: string[] = ['Train', 'Play the Guitar']
+const address2: {
+  street: string
+  city: string
+} = {
+  street: 'Main St',
+  city: 'New York'
+}
+
+// Template literals
+const introduction = `My name is ${person} and I am ${age} years old. I live in ${address.city} and I like to ${hobbies.join(', ')}.`
+console.log(introduction)
+
+
+
